90b60bef140a0aa09315958057ac9150ff89dc12
Thanh Quyen <voquyen090401@gmail.com>
2025-06-07 23:49:48 +0700

    fix: add trend

--- src/utils/check.js
+++ src/utils/check.js
@@ -1,30 +1,49 @@
 import { check } from 'k6';
-import { Trend } from 'k6/metrics';
+import { Trend, Counter } from 'k6/metrics';
 
 // Khai báo biến Trend để đo thời gian phản hồi
 export let responseTimeTrend = new Trend('response_time');
+export let successCount = new Counter('success_requests');
+export let failureCount = new Counter('failed_requests');
+
 
 export function checkBasic(module, response, maxDurationMs = 1000) {
-    checkStatus200(module, response)
-    checkResponseTime(module, response, maxDurationMs)
-
-    responseTimeTrend.add(response.timings.duration, { module: module });
+    checkStatus200(module, response);
+    checkResponseTime(module, response, maxDurationMs);
 }
 
 export function checkStatus200(module, response) {
-    const label = `[${module}] status is 200`
+    if (!response) {
+        console.error(`[${module}] Response is null or undefined`);
+        failureCount.add(1);
+        return;
+    }
+
+    const label = `[${module}] status is 200`;
+    const isSuccess = response?.status === 200;
+
     check(response, {
-        [`${label}`]: (r) => r?.status === 200,
+        [`${label}`]: () => isSuccess,
     });
 
-    responseTimeTrend.add(response?.timings?.duration, { module: module });
+    if (isSuccess) {
+        successCount.add(1);
+    } else {
+        failureCount.add(1);
+    }
 }
 
 export function checkResponseTime(module, response, maxDurationMs = 500) {
-    const label = `[${module}] response time < ${maxDurationMs}`
+    if (!response || !response.timings) {
+        console.error(`[${module}] Response or timings are null or undefined`);
+        failureCount.add(1);
+        return;
+    }
+
+    const label = `[${module}] response time < ${maxDurationMs}`;
     check(response, {
-        [`${label}`]: (r) => r?.timings?.duration < maxDurationMs,
+        [`${label}`]: (r) => r.timings.duration < maxDurationMs,
     });
 
-    responseTimeTrend.add(response?.timings?.duration, { module: module });
+    responseTimeTrend.add(response.timings.duration);
 }
